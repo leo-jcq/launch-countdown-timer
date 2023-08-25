444c34aef06560e02f4328b0c841d7871f3ed89e
Léo Jacquet <leojacquet2003@gmail.com>
2023-08-25 02:28:02 +0200

    🔧 fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/launch-countdown-timer.git/'
+    base: '/launch-countdown-timer/'
 });
