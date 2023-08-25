0a4c8052b868bc8eeea3aad53ed50a7dd8af91a0
Léo Jacquet <leojacquet2003@gmail.com>
2023-08-25 17:22:55 +0200

    ✨ add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
 import { FC } from 'react';
+import { iconFacebook, iconInstagram, iconPinterest } from '../../assets/assets';
 import './Footer.scss';
 
 const Footer: FC = () => {
-    return <footer className="footer">Footer</footer>;
+    return (
+        <footer className="footer">
+            <a href="https://facebook.com" target="_blank" className="image">
+                {iconFacebook}
+            </a>
+            <a href="https://pinterest.com" target="_blank" className="image">
+                {iconPinterest}
+            </a>
+            <a href="https://instagram.com" target="_blank" className="image">
+                {iconInstagram}
+            </a>
+        </footer>
+    );
 };
 
 export default Footer;
